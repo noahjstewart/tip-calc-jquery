73db3c11cf0959311293ed0aa7a19d6ab7afb0dc
Noah Stewart <noah.stewart9813@gmail.com>
2020-05-30 23:45:47 -0400

    Calculator functionality finished besides rest

--- src/Calculator.js
+++ src/Calculator.js
@@ -2,4 +2,81 @@ import $ from "jquery";
 
 export const Calculator = function (sel) {
   var that = this;
+  this.div = $(sel);
+  this.errornous = false;
+
+  this.form = this.div.find("#form").html(`
+    <form>
+      <p>
+        <label for="bill">Bill Amount:</label>
+        <input type="text" id="bill" required>
+      </p>
+      <p>
+        <label for="bill">Tip Percent:</label>
+        <input type="number" id="tip" value="20">
+      </p>
+      <p>
+        <input id="calculate" type="submit" value="Calculate">
+      </p>
+    </form>
+  `);
+
+  this.form.submit((event) => {
+    event.preventDefault();
+    if (that.errornous) {
+      that.resetErrors();
+    }
+    that.calculate();
+  });
+
+  this.calculate = function () {
+    var billAmount = parseFloat($("#bill").val());
+    if (isNaN(billAmount)) {
+      this.addError("Bill amount must be a number");
+      this.errornous = true;
+      $("#bill").val("");
+    } else if (billAmount <= 0) {
+      this.addError("Bill amount must be greater than 0");
+      this.errornous = true;
+    }
+
+    var tipPercent = $("#tip").val() / 100;
+    if (tipPercent < 0) {
+      this.addError("Tip percent must be greater than 0");
+      this.errornous = true;
+    }
+
+    if (!this.errornous) {
+      var tipAmount = (billAmount * tipPercent).toFixed(2);
+      var totalAmount = (billAmount + parseFloat(tipAmount)).toFixed(2);
+
+      var resultHtml = `
+      <div class="spread">
+        <p>Tip Amount:</p>
+        <p>$${tipAmount}</p>
+      </div>
+      <div class="spread">
+        <p>Total Amount:</p>
+        <p>$${totalAmount}</p>
+      </div>
+      `;
+
+      this.div.find("#result").html(resultHtml);
+    }
+  };
+
+  this.resetErrors = function () {
+    this.errornous = false;
+    $("#error-container").empty();
+  };
+
+  this.resetForm = function () {
+    $("#bill").val("");
+    $("#tip").val(20);
+  };
+
+  this.addError = function (msg) {
+    var errorMsg = `<p class="error">${msg}</p>`;
+    $("#error-container").append(errorMsg);
+  };
 };
